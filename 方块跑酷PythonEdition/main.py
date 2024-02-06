832661287af6ff167301aaf011af3136e2bfa65a
zbl201054 <158832416+zbl201054@users.noreply.github.com>
2024-02-06 22:47:58 +0800

    Add files via upload

--- 方块跑酷PythonEdition/main.py
+++ 方块跑酷PythonEdition/main.py
@@ -41,6 +41,12 @@ f=0
 g=0
 fh=Actor("fh.png")
 fh1=0
+cg=Actor("闯关模式.png",(600,250))
+zy=Actor("自由模式.png",(200,250))
+k=0
+color=0
+floor=Actor("black_block.png",(445,500))
+project_1=Actor("project0.png",(100,200))
 def draw():
     global m
     screen.clear()
@@ -73,6 +79,8 @@ def draw():
         dz.draw()
     if m=="游戏模式":
         fh.draw()
+        cg.draw()
+        zy.draw()
     if m=="公告":
         fh.draw()
     if m=="个人中心":
@@ -85,6 +93,10 @@ def draw():
         fh.draw()
     if m=="设置":
         fh.draw()
+    if m=="第一关":
+        fh.draw()
+        project_1.draw()
+        floor.draw()
 def new0():
     global m
     m = '加载'
@@ -116,6 +128,9 @@ def new8():
 def new9():
     global m
     m="设置"
+def new10():
+    global m
+    m="第一关"
 def jd0():
     global i #全局变量
     if i < 6:
@@ -125,7 +140,13 @@ def dl0():
     m="主屏幕"
 
 def update():
-    global m, i,dl2,fh1,a,b,c,d,e,f,g
+    global m, i,dl2,fh1,a,b,c,d,e,f,g,k,color
+    f4=open("color.txt","w",encoding="utf-8")
+    f4.write(str(color))
+    f4.close()
+    f5=open("color.txt","r",encoding="utf-8")
+    color=int(f5.read())
+    f5.close()
     if fh1==1:
         clock.schedule(dl0,1)
     if (m == '开始'):
@@ -142,6 +163,7 @@ def update():
             clock.schedule(dl0,1)
     if m=="主屏幕":
         fh1=0
+        project.image="project"+str(color)+".png"
         if a==1:
             clock.schedule(new3,1)
         if b==1:
@@ -158,6 +180,8 @@ def update():
             clock.schedule(new9,1)
     if m=="游戏模式":
         a=0
+        if k==1:
+            clock.schedule(new10,1)       
     if m=="公告":
         b=0
     if m=="个人中心":
@@ -170,8 +194,15 @@ def update():
         f=0
     if m=="设置":
         g=0
+    if m=="第一关":
+        k=0
+        project_1.image="project"+str(color)+"_1"+".png"
+        #project.pos=(100,200)
+        project_1.y+=5
+        if project_1.y>=465:
+            project_1.y-=5
 def on_mouse_down(pos):
-    global m,f0,f1,f2,f3,dl2,dl1,q,a,b,c,d,e,f,fh1,g
+    global m,f0,f1,f2,f3,dl2,dl1,q,a,b,c,d,e,f,fh1,g,k
     if fh.collidepoint(pos):
         fh1=1
     if m=="登录":
@@ -215,4 +246,7 @@ def on_mouse_down(pos):
             f=1
         if settings.collidepoint(pos):
             g=1
+    if m=="游戏模式":
+        if zy.collidepoint(pos):
+            k=1    
 go()
